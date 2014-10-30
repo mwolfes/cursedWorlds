5264ceedadfad1cc7a92b9507349c8b8e924656a
M <m@w.de>
2014-10-30 19:21:00 +0100

    started new window manager

--- cursedWorlds.py
+++ cursedWorlds.py
@@ -5,43 +5,20 @@ import traceback
 import curses
 
 cw = cWinManager()
-player = world.player(10,20,"mesiter")
-theSetting = world.setting([player])
+#player = world.player(10,20,"mesiter")
+#theSetting = world.setting([player])
 
 def loop():
-    mx = 0
-    my = 0
+    #mx = 0
+    #my = 0
     try:
         while True:
             key = cw.screen.getch()
-            if key==curses.KEY_RESIZE:
-                cw.resizeTerm()
+            #if key==curses.KEY_RESIZE:
+                #cw.resizeTerm()
 
-            if key==ord('a'):
-                mx+=2
-                cw.refreshMap(my,mx,theSetting)
-            if key==ord('d'):
-                mx+=-2
-                cw.refreshMap(my,mx,theSetting)
-            if key==ord('w'):
-                my+=-1
-                cw.refreshMap(my,mx,theSetting)
-            if key==ord('s'):
-                my+=1
-                cw.refreshMap(my,mx,theSetting)
-
-            if key==ord('l'):
-                player.x+=2
-                cw.refreshMap(my,mx,theSetting)
-            if key==ord('h'):
-                player.x+=-2
-                cw.refreshMap(my,mx,theSetting)
-            if key==ord('k'):
-                player.y+=-1
-                cw.refreshMap(my,mx,theSetting)
-            if key==ord('j'):
-                player.y+=1
-                cw.refreshMap(my,mx,theSetting)
+            if key==ord('r'):
+                cw.refresh()
 
             if key==ord('q'):
                 break
--- lib/ui.py
+++ lib/ui.py
@@ -4,17 +4,31 @@ from lib import world
 
 
 class cWin:
-    def __init__(self, parent, level = 0, posy = 0.0, posx = 0.0 , sizey = 1.0, sizex = 1.0 ):
+    def __init__(self, parent, posy = 0.0, posx = 0.0 , sizey = 1.0, sizex = 1.0, level = 0 ):
         self.parent = parent
         self.level = level
         self.posy = posy
         self.posx = posx
         self.sizey = sizey
         self.sizex = sizex
+        self.spacing = 1
 
         my, mx = self.parent.getmaxyx()
+        yy = int(my * posy) + self.spacing
+        xx = int(mx * posx) + self.spacing
+        wy = int(my * sizey) - yy - self.spacing
+        wx = int(mx * sizex) - xx - self.spacing
 
-        self.win = curses.newwin(int(my * sizey),int(mx*sizex),int(my * posy),int(mx * posx))
+        self.win = curses.newwin(wy,wx,yy,xx)
+
+    def refresh(self):
+        self.win.noutrefresh()
+
+class cWbox(cWin):
+    def refresh(self):
+        self.win.box()
+        self.win.noutrefresh()
+
         
 class cWinManager:
     def __init__(self):
@@ -23,13 +37,22 @@ class cWinManager:
         curses.noecho()
         curses.cbreak()
         curses.curs_set(0)
-        if curses.has_colors():
-            curses.start_color()
+        #if curses.has_colors():
+            #curses.start_color()
         self.screen.keypad(1)
         self.screen.box()
 
-        self.map = curses.newpad(75,75)
-        self.map.box()
+        self.Map = cWbox(self.screen,0.0,0.0,0.8,0.8)
+        self.winlist = [self.Map]
+
+#       self.map = curses.newpad(75,75)
+#       self.map.box()
+
+    def refresh(self):
+        self.screen.noutrefresh()
+        for win in self.winlist:
+            win.refresh()
+        curses.doupdate()
 
     def quit(self):
         self.screen.keypad(0)
@@ -38,23 +61,24 @@ class cWinManager:
         curses.nocbreak()
         curses.endwin()
 
-    def refreshMap(self,my,mx,setting):
-        self.screen.erase()
-        self.screen.box()
-        self.screen.refresh()
-        #redraw the map
-        self.map.erase()
-        self.map.box() #for testing, remove at the end??? 
-        #place all objects
-        for entity in setting.entities:
-            if entity.getShape():
-                self.map.addstr(entity.y,entity.x,entity.getShape()[0])
-        self.map.refresh(my,mx,1,1,self.maxy - 3,self.maxx-2)
+#   def refreshMap(self,my,mx,setting):
+#       self.screen.erase()
+#       self.screen.box()
+#       self.screen.refresh()
+#       #redraw the map
+#       self.map.erase()
+#       self.map.box() #for testing, remove at the end??? 
+#       #place all objects
+#       for entity in setting.entities:
+#           if entity.getShape():
+#               self.map.addstr(entity.y,entity.x,entity.getShape()[0])
+#       self.map.refresh(my,mx,1,1,self.maxy - 3,self.maxx-2)
+#
+#   def resizeTerm(self):
+#       self.maxy,self.maxx = self.screen.getmaxyx()
+#       self.screen.clear()
+#       curses.resizeterm(maxy,maxx)
+#       self.screen.box()
+#       self.screen.refresh()
 
-    def resizeTerm(self):
-        self.maxy,self.maxx = self.screen.getmaxyx()
-        self.screen.clear()
-        curses.resizeterm(maxy,maxx)
-        self.screen.box()
-        self.screen.refresh()
 
